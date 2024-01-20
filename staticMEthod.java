0b996320ae4478f3024118c5287e6de34980d87f
Code1123 <dattabalbudhe1@gmail.com>
2024-01-20 16:53:53 +0530

    staticmethod1

--- staticMEthod.java
+++ staticMEthod.java
@@ -1,3 +1,23 @@
-public class staticMEthod {
+public class staticMEthod 
+{
     
-}
+    static void display()
+    {
+        System.out.println("Welcome");
+    }
+    public static void main(String[] args) 
+    {
+        display();                                 // directly  
+
+        staticMEthod.display();                   // By using class Name
+        
+        new staticMEthod().display();            // By using an Object
+        
+        staticMEthod st=new staticMEthod();      //By using an object refernce
+        st.display();
+    }
+    
+        
+        
+ }
+
