d6e1f10b3a9f5d33f31045a22586b68d02f3ad73
Code1123 <dattabalbudhe1@gmail.com>
2024-01-20 16:53:34 +0530

    methodOverloadingex1

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -1,3 +1,15 @@
 public class MethodOverloading {
-    
+    void add(int a,int b)
+    {
+        System.out.println(a+b);
+    }
+    void add(int a,int b,int c)
+    {
+        System.out.println(a+b+c);
+    }
+    public static void main(String[] args) {
+        MethodOverloading md=new MethodOverloading();
+        md.add(10, 50);
+        md.add(40, 7, 3);
+    }
 }
