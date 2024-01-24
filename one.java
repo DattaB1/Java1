8e93313cedfb52feea57b9ad9dab89c50c38f924
Code1123 <dattabalbudhe1@gmail.com>
2024-01-24 12:29:36 +0530

    this_keyword

--- one.java
+++ one.java
@@ -1,5 +1,8 @@
-public class one {
-    int x=10;
+public class one 
+{
+
+    //this keyword
+   /*  int x=10;
     void show()
     {
         System.out.println(this.x);
@@ -8,5 +11,39 @@ public class one {
         one n=new one();
         n.show();
     }
+    */
+
+
+
+// this keyword refers currnt objcet..in below example two o/p are comes i called with first obj i.e d1 so it give me first and second is same.
+    /*int x=10;
+    void show()
+    {   
+        System.out.println(x);
+    } 
+    public static void main(String[] args) 
+    {
+        one obj=new one();
+        one obj2=new one();
+        obj.x=obj.x+3;
+        obj.show();
+        obj2.show();
+    }*/
+
+
+// in this example there is local and instnce varaible are present..so if we want to access instance variable.then this keyword must
+  //be important.and 2nd local varaible is directly assess
+  int x=10;                     //instance variable
+  public void show()
+  {
+    int x=20;                      //local varaible
+    System.out.println(x);
+    System.out.println(this.x);
+  }
+  public static void main(String[] args) {
+    one obj=new one();
+    obj.show();
+  }
+
     
 }
