edaa61d1f6500d5de5cd5d14972d9d9e624cc647
Code1123 <dattabalbudhe1@gmail.com>
2024-01-24 12:29:16 +0530

    this keyword

---
+++ a.java
@@ -0,0 +1,29 @@
+public class a{
+    //public static void main(String[] args) {
+        /*int n=0;
+         if(n>0){
+            System.out.println(n+"posi");
+         }else{
+            System.out.println(n+"neg");
+         }*/
+     /*    int x=10;
+         void show()
+         {
+            System.out.println(d.x);
+         }
+   public static void main(String[] args) {
+      a obj=new A();
+      obj.x;
+   } */
+   int x=10;
+   void show{
+
+      System.out.println(x);
+   }
+public static void main(String[] args) {
+   A obj=new A();
+   obj.show();
+   
+}
+    
+}
---
+++ one.java
@@ -0,0 +1,12 @@
+public class one {
+    int x=10;
+    void show()
+    {
+        System.out.println(this.x);
+    }
+    public static void main(String[] args) {
+        one n=new one();
+        n.show();
+    }
+    
+}
