15fed7ffddd28ba1fe0e44274e7ab4ac6aad5b19
Code1123 <dattabalbudhe1@gmail.com>
2024-03-04 22:46:25 +0530

    garbagevaluebyArray

---
+++ Arrryabasic1.java
@@ -0,0 +1,13 @@
+public class Arrryabasic1 {
+
+  public static void main(String[] args) {
+    //type arrayname[]=new type[size];
+    int arr[]=new int[5];
+    arr[0]=35;
+    arr[1]=45;
+    arr[2]=78;
+    System.out.println(arr); 
+  }
+}
+
+// output is garbage value:----hashcode mixcture
