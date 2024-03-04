d3b85b91062e9878ad40526acc2790316b060dfe
Code1123 <dattabalbudhe1@gmail.com>
2024-03-04 22:53:28 +0530

    ArrayFirstExample

---
+++ ArrayBasic2.java
@@ -0,0 +1,15 @@
+public class ArrayBasic2 {
+  public static void main(String[] args) {
+    int arra[]=new int[5];
+    arra[0]=54;
+    arra[1]=85;
+    arra[2]=45;
+    arra[3]=75;
+    arra[4]=71;
+    //arra[5]=42;
+    for(int i=0; i<=arra.length; i++)
+    {
+      System.out.println(arra[i] );
+    }
+  }
+}
