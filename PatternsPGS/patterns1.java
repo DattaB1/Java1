25cf39cde799e1e419f19e945a0167f6c528041c
Code1123 <dattabalbudhe1@gmail.com>
2024-03-16 11:48:10 +0530

    patter1Simple

---
+++ PatternsPGS/patterns1.java
@@ -0,0 +1,20 @@
+package PatternsPGS;
+
+public class patterns1 
+{
+  public static void main(String[] args) {
+
+    //outer loop for rows
+    for(int i=1;i<=10;i++)
+    {
+
+          //inner for coloums    
+          for(int j=1;j<=i;j++)     //colum depend on rows
+          {
+            System.out.print("*");
+          }
+          System.out.println();
+
+    }
+  }
+}
