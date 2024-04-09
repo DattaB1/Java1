e11f6f7beb2ad0b6eafe182122d380ce08f3736d
Code1123 <dattabalbudhe1@gmail.com>
2024-04-09 12:36:47 +0530

    print the maximum of SubArray

--- DSA_PGS/PrintMaxSubArray.java
+++ DSA_PGS/PrintMaxSubArray.java
@@ -1,9 +1,31 @@
 public class PrintMaxSubArray {
-  public static void printSubArrayMaxsum(int num[])
+  public static void maxSubArraySum(int num[])
   {
-    
+       int currSum=0;
+       int maxSum=Integer.MIN_VALUE;
+      
+       for(int i=0;i<num.length;i++)
+       {
+        int start=i;
+        for(int j=i;j<num.length;j++)
+        {
+          int end=j;
+          currSum=0;
+          for(int k=start;k<=end;k++)
+          {
+            //printSubArraySUM
+            currSum+=num[k];
+          }
+          System.out.println(currSum);
+          if(maxSum<currSum){
+              maxSum=currSum;
+          }
+        }
+       }
+       System.out.println("max sum="+maxSum);
   }
   public static void main(String[] args) {
-    int num[]={2,4,6,8,10};
+    int num[]={1,-2,6,-1,3};
+    maxSubArraySum(num);
   }
 }
